b5d6dd1bbc6d33da224b61ff20f280dbb0ad0df1
Alex Shulgin <alex.shulgin@gmail.com>
2022-01-29 09:38:22 +0100

    Enable interactive use and add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Supaplex @ Emacs
+
+1. Find file `LEVELS.DAT`.
+2. `M-x supa-mode`.
+3. `M-x supa-edit-level` and enter the level number.
+4. `M-x supa-list-levels` to go back to the levels list.
+
+Tiles image taken from:
+https://github.com/mokyu/supaplex-level-editor/blob/master/resources/tiles.png
--- supa.el
+++ supa.el
@@ -1,11 +1,11 @@
-(defimage tiles
-  ((:type png :file "~/Downloads/GAMES/supaplex/tiles.png")))
+(defimage supa-tiles-image
+  ((:type png :file "~/src/supa-el/tiles.png")))
+;; (setq supa-tiles-image (find-image '((:type png :file "~/src/supa-el/tiles.png"))))
 
-(defconst levels-buf (get-buffer "LEVELS.DAT"))
+(defconst supa-zero-height-newline (propertize "\n" 'face '(:height 0)))
 
-(defconst zero-height-newline (propertize "\n" 'face '(:height 0)))
-
-(defun list-levels ()
+(defun supa-list-levels ()
+  (interactive)
   (remove-overlays)
   (widen)
   (with-silent-modifications
@@ -16,7 +16,8 @@
              (name (buffer-substring (+ s 1446) (+ s 1469))))
         (put-text-property s e 'display (format "%03d: %s\n" (1+ lvl) name))))))
 
-(defun edit-level (lvl)
+(defun supa-edit-level (lvl)
+  (interactive "nLevel: ")
   (remove-overlays)
   (widen)
   (let* ((l (1+ (* 1536 (1- lvl))))
@@ -31,64 +32,57 @@
         (let* ((line (+ l (* 60 i)))
                (lend (+ line 60))
                (o    (make-overlay lend lend)))
-          (overlay-put o 'before-string zero-height-newline)
+          (overlay-put o 'before-string supa-zero-height-newline)
           (dotimes (j 60)
             (let* ((s (+ line j))
                    (e (1+ s))
                    (c (aref level-contents (+ (* 60 i) j))))
-              (put-text-property s e 'display (list tiles (list 'slice (* 16 c) 0 16 16)))))))
+              (put-text-property s e 'display (list supa-tiles-image (list 'slice (* 16 c) 0 16 16)))))))
       (set-text-properties meta        (+ meta 96) nil)
       (put-text-property   meta        (+ meta  7) 'display (format "%03d " lvl))
       (put-text-property   (+ meta 29) (+ meta 30) 'display (format " %03d / %03d " 0 info-cnt))
-      (put-text-property   (+ meta 30) (+ meta 31) 'display (list tiles '(slice 64 0 16 16)))
-      (put-text-property   (+ meta 31) (+ meta 96) 'invisible t))
-    ))
+      (put-text-property   (+ meta 30) (+ meta 31) 'display (list supa-tiles-image '(slice 64 0 16 16)))
+      (put-text-property   (+ meta 31) (+ meta 96) 'invisible t))))
 
 (define-derived-mode supa-mode
   fundamental-mode "Supa"
   "Major mode for Supaplex LEVELS.DAT file."
-  (list-levels))
+  (supa-list-levels))
 
 (define-minor-mode supa-level-mode
   "Minor mode for Supaplex level within the LEVELS.DAT file.")
 
-(with-current-buffer levels-buf
-  (list-levels))
+;; (with-current-buffer (make-indirect-buffer levels-buf "LEVELS.DAT@5" t)
+;;   (edit-level 5))
 
-(with-current-buffer levels-buf
-  (edit-level 5))
+;; (with-current-buffer levels-buf
+;;   (put-text-property 1 (point-max) 'invisible t))
 
-(with-current-buffer (make-indirect-buffer levels-buf "LEVELS.DAT@5" t)
-  (edit-level 5))
+;; ;; fill line with an object
+;; (with-current-buffer levels-buf
+;;   (let ((n 58))
+;;     (delete-char n)
+;;     (dotimes (i n)
+;;       (insert-char 24))))
 
-(with-current-buffer levels-buf
-  (put-text-property 1 (point-max) 'invisible t))
+;; ;; make blank level
+;; (with-current-buffer levels-buf
+;;   (delete-region (+ 1536 61) (+ 1537 (* 23 60)))
+;;   (dotimes (i 22)
+;;     (insert-char 6)
+;;     (dotimes (j 58) (insert-char 2))
+;;     (insert-char 6))
+;;   ;
+;;   )
 
-;; fill line with an object
-(with-current-buffer levels-buf
-  (let ((n 58))
-    (delete-char n)
-    (dotimes (i n)
-      (insert-char 24))))
+;; ;; put all available objects in one line
+;; (with-current-buffer levels-buf
+;;   (delete-char 40)
+;;   (dotimes (i 40) (insert-char i)))
 
-;; make blank level
-(with-current-buffer levels-buf
-  (delete-region (+ 1536 61) (+ 1537 (* 23 60)))
-  (dotimes (i 22)
-    (insert-char 6)
-    (dotimes (j 58) (insert-char 2))
-    (insert-char 6))
-  ;
-  )
+;; (defconst over1
+;;   (with-current-buffer levels-buf
+;;     (make-overlay 61 61)))
 
-;; put all available objects in one line
-(with-current-buffer levels-buf
-  (delete-char 40)
-  (dotimes (i 40) (insert-char i)))
-
-(defconst over1
-  (with-current-buffer levels-buf
-    (make-overlay 61 61)))
-
-(overlay-put over1 'before-string "\n")
-(delete-overlay over1)
+;; (overlay-put over1 'before-string "\n")
+;; (delete-overlay over1)
