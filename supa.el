dc29a9f2b3f5e1f176850d77f4642939f26de1da
a1exsh <alex.shulgin@gmail.com>
2022-02-13 19:22:30 +0100

    Add dependency on 'dash

--- supa.el
+++ supa.el
@@ -16,6 +16,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;
+
+;; only used for the threading macros: ->, ->>
+(require 'dash)
+
 (defconst supa-level-max 111)
 (defconst supa-level-size-in-bytes 1536) ;; 3 disk sectors apparently
 
